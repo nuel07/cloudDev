3e723797fc513cdc8fad4a005495683eb388b02b
nuel07 <yehanganep@gmail.com>
2022-08-10 07:37:49 +0300

    updates

--- basic_server/tests/funcs.ts
+++
@@ -1,9 +0,0 @@
-// example functions for unit testing purposes
-export const add = (a: number, b: number) => {
-    return a + b;
-}
-
-export const divide = (a: number, b: number) => {
-    if ( b === 0 ) { throw new Error('Division by Zero')}
-    return a / b;
-}
