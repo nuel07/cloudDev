00f9e0a5ed1a7ebe61693d881d0d0f3ba3e1160a
nuel07 <yehanganep@gmail.com>
2022-08-15 14:27:04 +0300

    server.ts correction

--- basic_server/src/server.ts
+++ basic_server/src/server.ts
@@ -1,4 +1,4 @@
-import express, { Router, Request, Response } from 'express';
+import  express, { Router, Request, Response } from 'express';
 import bodyParser from 'body-parser';
 
 import { Car, cars as cars_list } from './cars';
