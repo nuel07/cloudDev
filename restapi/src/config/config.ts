795b1298f297b28748279b34342b8c0c9006835c
nuel07 <yehanganep@gmail.com>
2022-08-19 01:29:26 -0700

    updated packages

--- restapi/src/config/config.ts
+++ restapi/src/config/config.ts
@@ -1,13 +1,13 @@
 export const config = {
   "dev": {
-    "username": "",
-    "password": "",
-    "database": "",
-    "host": "",
+    "username": "nuel",
+    "password": "dererama",
+    "database": "nuel_udagram",
+    "host": "nuel-udagram.cznbcwqa3htj.us-east-1.rds.amazonaws.com",
     "dialect": "postgres",
-    "aws_region": "us-east-2",
+    "aws_region": "us-east-1",
     "aws_profile": "default",
-    "aws_media_bucket": "udagram-ruttner-dev"
+    "aws_media_bucket": "nuelbucketp2"
   },
   "jwt": {
     "secret": " "
