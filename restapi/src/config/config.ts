2be711dc48f658b9abba72a945b717be579b4b48
nuel07 <yehanganep@gmail.com>
2022-08-26 07:31:00 +0300

    updates

--- restapi/src/config/config.ts
+++ restapi/src/config/config.ts
@@ -1,16 +1,16 @@
 export const config = {
   "dev": {
-    "username": "nuel",
-    "password": "dererama",
-    "database": "nuel_udagram",
-    "host": "nuel-udagram.cznbcwqa3htj.us-east-1.rds.amazonaws.com",
+    "username": process.env.POSTGRES_USERNAME,
+    "password": process.env.POSTGRES_PASSWORD,
+    "database": process.env.POSTGRES_DATABASE,
+    "host": process.env.POSTGRES_HOST,
     "dialect": "postgres",
-    "aws_region": "us-east-1",
-    "aws_profile": "default",
-    "aws_media_bucket": "nuelbucketp2"
+    "aws_region": process.env.AWS_REGION,
+    "aws_profile": process.env.AWS_PROFILE,
+    "aws_media_bucket": process.env.AWS_MEDIA_BUCKET,
   },
   "jwt": {
-    "secret": " "
+    "secret": process.env.JWT_SECRET,
   },
   "prod": {
     "username": "",
